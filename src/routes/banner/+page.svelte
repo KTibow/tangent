75a6b1d1e2a81456099d11cf6e4b35606b9dd066
KTibow <KTibow@users.noreply.github.com>
2025-06-24 14:53:02 -0700

    page to generate banner

---
+++ src/routes/banner/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { iconTangent } from "$lib/icons";
+  import TangentContainer from "$lib/TangentContainer.svelte";
+  import Icon from "$lib/Icon.svelte";
+</script>
+
+<TangentContainer>
+  <h1 class="m3-font-headline-large">
+    <Icon icon={iconTangent} width="2rem" height="2rem" />
+    Tangent
+  </h1>
+</TangentContainer>
+
+<style>
+  h1 {
+    display: inline-flex;
+    align-items: center;
+    gap: 0.5rem;
+    color: rgb(var(--m3-scheme-primary));
+    margin: auto;
+    font-weight: bold;
+  }
+</style>
