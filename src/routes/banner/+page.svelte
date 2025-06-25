a6f09a5d9de86be3c77f2240c4e753ccfbb17e50
KTibow <KTibow@users.noreply.github.com>
2025-06-25 13:31:54 -0700

    minor branding/banner tweak

--- src/routes/banner/+page.svelte
+++ src/routes/banner/+page.svelte
@@ -5,19 +5,22 @@
 </script>
 
 <TangentContainer>
-  <h1 class="m3-font-headline-large">
-    <Icon icon={iconTangent} width="2rem" height="2rem" />
+  <h1 class="m3-font-display-large">
+    <Icon icon={iconTangent} width="6rem" height="6rem" />
     Tangent
   </h1>
 </TangentContainer>
 
 <style>
+  /* Best viewed at 1280x640 */
   h1 {
     display: inline-flex;
     align-items: center;
-    gap: 0.5rem;
+    gap: 0.125em;
     color: rgb(var(--m3-scheme-primary));
     margin: auto;
+
+    font-size: 6rem;
     font-weight: bold;
   }
 </style>
