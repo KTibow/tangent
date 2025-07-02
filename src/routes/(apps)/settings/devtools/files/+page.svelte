acefaf2aec4c137bc3d9deac1579240f1201cfd8
KTibow <KTibow@users.noreply.github.com>
2025-07-02 11:10:06 -0700

    delete button

--- src/routes/(apps)/settings/devtools/files/+page.svelte
+++ src/routes/(apps)/settings/devtools/files/+page.svelte
@@ -7,7 +7,10 @@
 
 <ul>
   {#each files as f (f)}
-    <li>{f} ({storage[f].length}b)</li>
+    <li>
+      {f} ({storage[f].length}b)
+      <button onclick={() => delete storage[f]}>Delete</button>
+    </li>
   {/each}
 </ul>
 <p>{files.length} {files.length == 1 ? "file" : "files"}</p>
@@ -16,9 +19,14 @@
   li {
     display: flex;
     align-items: center;
+    gap: 2rem;
     height: 3rem;
     &:not(:first-child) {
       border-top: 1px solid var(--m3-scheme-outline);
+    }
+    > button {
+      align-self: stretch;
+      color: rgb(var(--m3-scheme-primary));
     }
   }
   p {
